b4b9a81db11ab24cd44fbe1cbe2e5dcb396713ab
calebcorrigan <caleb.corrigan@aol.com>
2022-06-28 10:41:32 -0400

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Caleb Corrigan on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
